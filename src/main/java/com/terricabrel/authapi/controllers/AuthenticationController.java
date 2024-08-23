cf54fb4d9fde673e8d81023eeb96351b8df4d097
Enrico <bregolienrico@gmail.com>
2024-08-23 23:43:59 +0200

    [Rest Controller] implementato rest controller per registrazione e autenticazione

---
+++ src/main/java/com/terricabrel/authapi/controllers/AuthenticationController.java
@@ -0,0 +1,65 @@
+package com.terricabrel.authapi.controllers;
+
+import com.terricabrel.authapi.dtos.LoginUserDto;
+import com.terricabrel.authapi.dtos.RegisterUserDto;
+import com.terricabrel.authapi.entities.User;
+import com.terricabrel.authapi.services.AuthenticationService;
+import com.terricabrel.authapi.services.JwtService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/auth")
+@RestController
+public class AuthenticationController {
+
+    private final JwtService jwtService;
+    private final AuthenticationService authenticationService;
+
+    public AuthenticationController(JwtService jwtService, AuthenticationService authenticationService) {
+        this.jwtService = jwtService;
+        this.authenticationService = authenticationService;
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<User> register(@RequestBody RegisterUserDto registerUserDto) {
+        User registeredUser = authenticationService.signup(registerUserDto);
+
+        return ResponseEntity.ok(registeredUser);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> authenticate(@RequestBody LoginUserDto loginUserDto) {
+        User authenticatedUser = authenticationService.autheticate(loginUserDto);
+        String jwtToken = jwtService.generateToken(authenticatedUser);
+
+        LoginResponse loginResponse = new LoginResponse();
+        loginResponse.setToken(jwtToken);
+        loginResponse.setExpiresIn(jwtService.getExpirationTime());
+        
+        return ResponseEntity.ok(loginResponse);
+    }
+
+    public static class LoginResponse {
+        private String token;
+        private long expiresIn;
+
+        public String getToken() {
+            return token;
+        }
+
+        public void setToken(String token) {
+            this.token = token;
+        }
+
+        public long getExpiresIn() {
+            return expiresIn;
+        }
+
+        public void setExpiresIn(long expiresIn) {
+            this.expiresIn = expiresIn;
+        }
+    }
+}

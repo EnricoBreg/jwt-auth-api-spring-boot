4044527b828a56ac03b76ecf8c714a961833d13c
Enrico <bregolienrico@gmail.com>
2024-08-23 15:14:31 +0200

    [Entity e Repository] Aggiunta entity User e relativo repository

---
+++ src/main/java/com/terricabrel/auth_api/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.terricabrel.auth_api.repositories;
+
+import com.terricabrel.auth_api.entities.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+}

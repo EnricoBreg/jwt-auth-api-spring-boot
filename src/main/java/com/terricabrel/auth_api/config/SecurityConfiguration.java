c5885720a1794413299e1b087f0f8b15e30aa5f8
Enrico <bregolienrico@gmail.com>
2024-08-23 16:45:09 +0200

    [Security Config] Aggiunta logica per permettere o meno certe richieste HTTP. Lasciata vecchia sintassi di configurazione per confronto con la nuova

---
+++ src/main/java/com/terricabrel/auth_api/config/SecurityConfiguration.java
@@ -0,0 +1,90 @@
+package com.terricabrel.auth_api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    private final AuthenticationProvider authenticationProvider;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+
+    public SecurityConfiguration(AuthenticationProvider authenticationProvider, JwtAuthenticationFilter jwtAuthenticationFilter) {
+        this.authenticationProvider = authenticationProvider;
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
+
+    /**
+     * Si vogliono seguire i seguenti criteri:
+     * <ul>
+     *     <li>Non è necessario fornire il token CSRF perché lo useremo noi.</li>
+     *     <li>Le richieste con il percorso URL che corrisponde a /auth/signup e /auth/login non richiedono autenticazione.</li>
+     *     <li>Qualsiasi altro percorso URL di richiesta deve essere autenticato.</li>
+     *     <li>La richiesta è senza stato (stateless), il che significa che ogni richiesta deve essere trattata come nuova, anche se proviene dallo stesso client o è stata ricevuta in precedenza.</li>
+     *     <li>Deve essere utilizzato il provider di autenticazione personalizzato, e questo deve essere eseguito prima del middleware di autenticazione.</li>
+     *     <li>La configurazione CORS deve consentire solo richieste POST e GET.</li>
+     * </ul>
+     *
+     * @param http
+     * @return
+     * @throws Exception
+     */
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http.csrf()
+//                .disable()
+//                .authorizeHttpRequests()
+//                .requestMatchers("/auth/**")
+//                .permitAll()
+//                .anyRequest()
+//                .authenticated()
+//                .and()
+//                .sessionManagement()
+//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                .and()
+//                .authenticationProvider(authenticationProvider)
+//                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        http
+                .csrf(csrf -> csrf.disable()) // disaabilita CSRF
+                .authorizeHttpRequests(auth ->
+                        auth.requestMatchers("/auth/**").permitAll() // Permetti l'accesso a "/auth/**"
+                                .anyRequest().authenticated() // Tutte le altre richieste devono essere autenticate
+                )
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS) // Politica di gestione: STATELESS
+                )
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOrigins(List.of("http://localhost:8005"));
+        ;
+        configuration.setAllowedMethods(List.of("GET", "POST"));
+        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
+}

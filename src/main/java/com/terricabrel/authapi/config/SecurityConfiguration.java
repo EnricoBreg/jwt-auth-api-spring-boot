9e9abf810f0963f2bc4be0cdbea636ba417fcff8
Enrico <bregolienrico@gmail.com>
2024-08-24 17:22:44 +0200

    [Configuration] Aggiunta annotazione per abilitare l'uso delle varie annotazioni da usare nei controller per l'accesso basato sul ruolo

--- src/main/java/com/terricabrel/authapi/config/SecurityConfiguration.java
+++ src/main/java/com/terricabrel/authapi/config/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.terricabrel.authapi.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -16,11 +17,11 @@ import java.util.List;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity // per avere disponibili tutte le annotazioni da usare nei vari controller per l'accesso su ruolo
 public class SecurityConfiguration {
 
     private final AuthenticationProvider authenticationProvider;
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
-
 
     public SecurityConfiguration(AuthenticationProvider authenticationProvider, JwtAuthenticationFilter jwtAuthenticationFilter) {
         this.authenticationProvider = authenticationProvider;

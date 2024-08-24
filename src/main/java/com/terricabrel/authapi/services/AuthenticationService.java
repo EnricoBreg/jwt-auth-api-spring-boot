46dcfb4f575b2ba98f2a3827a2c422be4290e822
Enrico <bregolienrico@gmail.com>
2024-08-24 17:17:06 +0200

    [Service] Aggiunto ruole quando l'utente si registra

--- src/main/java/com/terricabrel/authapi/services/AuthenticationService.java
+++ src/main/java/com/terricabrel/authapi/services/AuthenticationService.java
@@ -2,32 +2,45 @@ package com.terricabrel.authapi.services;
 
 import com.terricabrel.authapi.dtos.LoginUserDto;
 import com.terricabrel.authapi.dtos.RegisterUserDto;
+import com.terricabrel.authapi.entities.Role;
 import com.terricabrel.authapi.entities.User;
+import com.terricabrel.authapi.entities.enums.RoleEnum;
+import com.terricabrel.authapi.repositories.RoleRepository;
 import com.terricabrel.authapi.repositories.UserRepository;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class AuthenticationService {
 
     private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
     private final PasswordEncoder passwordEncoder;
     private final AuthenticationManager authenticationManager;
 
-
-    public AuthenticationService(UserRepository userRepository, PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager) {
+    public AuthenticationService(UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager) {
         this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
         this.passwordEncoder = passwordEncoder;
         this.authenticationManager = authenticationManager;
     }
 
     public User signup(RegisterUserDto input) {
+        Optional<Role> optionalRole = roleRepository.findByName(RoleEnum.USER);
+
+        if (optionalRole.isEmpty()) {
+            // TODO: manca un fallback se dovesse mancare il ruolo
+            return null;
+        }
         User user = new User();
         user.setFullName(input.getFullName());
         user.setEmail(input.getEmail());
         user.setPassword(passwordEncoder.encode(input.getPassword()));
+        user.setRole(optionalRole.get());
 
         return userRepository.save(user);
     }
